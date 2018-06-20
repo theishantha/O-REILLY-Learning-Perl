b62e3f82e467488657fe2ad6aacc91fef85e24be
Ishantha Udara <ishanthaudara96@gmail.com>
2018-06-20 20:55:38 +0530

    daily com 2

--- 3/3.1.pl
+++ 3/3.1.pl
@@ -7,3 +7,8 @@ print "Enter some lines, then press Ctrl+D:\n"; #or maybe Ctrl-Z
 @lines = <STDIN>;
 @reverse_lines = reverse @lines;
 print @reverse_lines;
+
+
+
+print "Enter some lines, then press Ctrl-D:\n";
+print reverse
