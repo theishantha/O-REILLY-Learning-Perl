95dc9263c9ae1d5f1f22b875b093691471918a0a
Ishantha Udara <ishanthaudara96@gmail.com>
2018-06-20 20:35:21 +0530

    daily com 1

---
+++ 3/3.1.pl
@@ -0,0 +1,9 @@
+# script.pl by IS
+
+use 5.18.0;
+use warnings;
+
+print "Enter some lines, then press Ctrl+D:\n"; #or maybe Ctrl-Z
+@lines = <STDIN>;
+@reverse_lines = reverse @lines;
+print @reverse_lines;
